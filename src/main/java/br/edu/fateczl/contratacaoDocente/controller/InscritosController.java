1e0e783daae198964c31ba64d339b9cb03b8abf1
BrunoTresoldi <bruno.tresoldi@outlook.com>
2026-05-29 12:14:25 -0300

    Adicionado InscritosController e uma classe Ordenar provisoria

---
+++ src/main/java/br/edu/fateczl/contratacaoDocente/controller/InscritosController.java
@@ -0,0 +1,83 @@
+package br.edu.fateczl.contratacaoDocente.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import javax.swing.JTextField;
+
+import br.edu.fateczl.contratacaoDocente.model.Inscricao;
+import model.Lista;
+
+public class InscritosController implements ActionListener {
+	
+	
+	//Ver somente se clicando no botao vai mostrar todas as inscrições ou procurar por algum atributo
+	private JTextField tfCodigoDisciplinaConsulta;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		String cmd = e.getActionCommand();
+
+		if (cmd.equals("Consultar")) {
+			try {
+				consultar();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+	
+	//Cria uma lista a partir dos dados lidos do arquivo inscrições
+	private void consultar() throws Exception {
+
+		String path = System.getProperty("user.home") + File.separator + "SistemaCadastro";
+		File arq = new File(path, "inscricoes.csv");
+
+		Lista<Inscricao> lista = new Lista<>();
+
+		if (arq.exists() && arq.isFile()) {
+			FileInputStream fis = new FileInputStream(arq);
+			InputStreamReader isr = new InputStreamReader(fis);
+			BufferedReader buffer = new BufferedReader(isr);
+			String linha = buffer.readLine();
+
+			while (linha != null) {
+				// Divide a String por ; e insere no vetor vetLinha
+				String[] vetLinha = linha.split(";");
+
+				Inscricao inscri = new Inscricao();
+
+				// Popular a Lista com cada objeto lido do arquivo
+
+				inscri.codDisciplina = vetLinha[0];
+				inscri.codProcesso = vetLinha[1];
+				inscri.cpfProfessor = vetLinha[2];
+			
+				lista.addLast(inscri);
+
+				linha = buffer.readLine();
+
+			}
+
+			buffer.close();
+			isr.close();
+			fis.close();
+
+		}
+		
+		ordena(lista);
+	}
+	
+	//Lista deve ser ordenada pela pontuação e as informações devem ser printadas no TextArea!!!
+	private void ordena(Lista<Inscricao> lista) {
+		
+	}
+	
+	
+
+}
---
+++ src/main/java/br/edu/fateczl/contratacaoDocente/controller/Ordenar.java
@@ -0,0 +1,74 @@
+package br.edu.fateczl.contratacaoDocente.controller;
+
+import br.edu.fateczl.contratacaoDocente.model.Professor;
+
+import model.Lista;
+
+public class Ordenar {
+	
+	public Ordenar() {
+		
+		super();
+		
+	}
+	
+	public Lista<Professor> quickSort(Lista<Professor> lista, int inicio, int fim) {
+
+		if (fim > inicio) {
+
+			int pivoFixo = dividir(lista, inicio, fim);
+			quickSort(lista, inicio, pivoFixo - 1); // divisão esquerda
+			quickSort(lista, pivoFixo + 1, fim); // divisão direita
+
+		}
+
+		return lista;
+	}
+
+	private int dividir(Lista<Professor> lista, int inicio, int fim) {
+
+		Professor pivo = lista.get(inicio);
+		int ponteiroEsquerda = inicio + 1;
+		int ponteiroDireita = fim;
+
+		// enquanto os ponteiros não se cruzarem
+
+		while (ponteiroEsquerda <= ponteiroDireita) {
+
+			while (ponteiroEsquerda <= ponteiroDireita && lista.get(ponteiroEsquerda).pontuacao <= pivo.pontuacao) {
+				ponteiroEsquerda++;
+			}
+			while (ponteiroDireita >= ponteiroEsquerda && lista.get(ponteiroDireita).pontuacao > pivo.pontuacao) {
+				ponteiroDireita--;
+			}
+			if (ponteiroEsquerda < ponteiroDireita) {
+				// os valores trocam de lugar
+				trocar(lista, ponteiroEsquerda, ponteiroDireita);
+
+				ponteiroEsquerda++;
+				ponteiroDireita--;
+
+			}
+
+		}
+
+		trocar(lista, inicio, ponteiroDireita);
+		return ponteiroDireita; // vai retornar a posição que já está fixada e ordenada e a partir desse valor
+								// consigo subdividir em vetores de esquerda e direita
+
+	}
+
+	// Criando um método de troca simples;
+
+	private void trocar(Lista<Professor> lista, int i, int j) {
+
+		Professor aux = lista.get(i);
+
+		lista.set(i,lista.get(j));
+
+		lista.set(j, aux);
+
+	}
+
+
+}
